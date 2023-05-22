056f1e60187f4b44518dcad61024ac27ccc26417
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-22 23:19:23 +0600

    Sheet #2 (Loops), problem: (E) Max

--- E_Max.cpp
+++ E_Max.cpp
@@ -21,6 +21,27 @@ using namespace std;
 int main()
 {
     FIO;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int mx = INT_MIN;
+
+     for (int i = 0; i < n; i++)
+    {
+        if(a[i]>mx) mx = max(mx,a[i]); 
+    }
+
+    //int mx = *max_element(a,a+n);
+    cout << mx << endl;
+
+    // int index = max_element(a,a+n)-a;
+
+    // cout << a[index] << endl;
      
     return 0;
 }
